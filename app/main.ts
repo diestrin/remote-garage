7e65390353ce1cabee4615d8b41f2cd729479d7d
Diego Barahona <diestrin@gmail.com>
2016-08-15 00:00:44 -0600

    Fixed typo on main.ts

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,7 @@ import {nativeScriptBootstrap} from 'nativescript-angular/application';
 // src imports
 import {AppComponent} from './app.component';
 import {setStatusBarColors} from './utils/status-bar-util';
-import {APP_ROUTER_PROVIDERS} from ' ./app.routes';
+import {APP_ROUTER_PROVIDERS} from './app.routes';
 
 setStatusBarColors();
 nativeScriptBootstrap(AppComponent, [
